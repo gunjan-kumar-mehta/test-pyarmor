db5b0c50484a43003a3cfb9b753d291a0f4823d6
Gunjan kumar mehta <gunjan.mehta@scio.technology>
2020-05-18 14:21:09 +0530

    sub and add

---
+++ run.py
@@ -0,0 +1,5 @@
+from op import add
+import subtract 
+
+add.add()
+subtract.subtract()
---
+++ subtract.py
@@ -0,0 +1,9 @@
+def subtract():
+        num1 = input('Enter first number: ')
+        num2 = input('Enter second number: ')
+
+        # Add two numbers
+        sub = float(num1) - float(num2)
+
+        # Display the subtract
+        print('The subtaction of {0} and {1} is {2}'.format(num1, num2, sub))
